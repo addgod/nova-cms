7c3f4f32a857351ee00306059240dd2809329d0b
Jesper Kold-Hansen <jkoldhansen@gmail.com>
2020-01-29 11:12:12 +0100

    Fixed a php 7.3 plus problem

--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,5 @@ use Illuminate\Support\Str;
 use Laravel\Nova\Nova;
 
 Route::get('{slug?}', 'PageController@show')
-    ->where('slug', '((?!' . Str::substr(Nova::path(), 1, -1) . '|nova)[A-z\d-\/_.]+)?')
+    ->where('slug', '((?!' . Str::substr(Nova::path(), 1, -1) . '|nova)[A-z\d\-\/_.]+)?')
     ->name('page.show');
